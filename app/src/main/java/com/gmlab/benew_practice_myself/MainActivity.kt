0cb98823d3cb2bc7dc690cb00d4b5d3adb3b024d
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2023-11-06 11:46:36 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/gmlab/benew_practice_myself/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmlab.benew_practice_myself
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
